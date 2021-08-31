4386d02a3f9bb51af4f61648837fea472c49b85b
boykis82 <boykis82@gmail.com>
2021-08-31 14:18:27 +0900

    2021-08-31 개별 빌드 한번에

--- build_all.bash
+++ build_all.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd util
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
@@ -9,26 +10,31 @@ cd microservices
 cd legacy-customer-service
 
 cd legacy-customer-client
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
 cd legacy-customer-server
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
 cd ..
 
 cd new-customer-server
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
 cd spring-cloud
 
 cd gateway
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
 cd eureka
+chmod 764 gradlew
 ./gradlew build
 cd ..
 
